f0faa22d45723dc4c268c7db5c0b0c3b983b0433
lsevcikova <48294719+lsevcikova@users.noreply.github.com>
2023-10-17 19:49:41 +0200

    updated stage and wordbox

--- src/components/Stage/index.jsx
+++ src/components/Stage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Wordbox from '../Wordbox';
 import wordList from '../../word-list';
 import './style.css';
@@ -7,24 +7,46 @@ const generateWord = (size) => {
   const sizeIndex = size === undefined
     ? Math.floor(Math.random() * wordList.length)
     : size - 3;
-  
+
   if (sizeIndex < 0 || sizeIndex >= wordList.length) {
     return null;
   }
-  
+
   const words = wordList[sizeIndex];
   const wordIndex = Math.floor(Math.random() * words.length);
   return words[wordIndex];
 };
 
 const Stage = () => {
-  const [words, setWords] = useState(['jahoda']);
+  const [words, setWords] = useState([]);
+  const [mistakes, setMistakes] = useState(0);
+
+  const handleFinish = () => {
+    // odstrani prvni slovo ze seznamu
+    const newWords = [...words];
+    newWords.shift();
+
+    // nove slovo a pridani na konec seznamu
+    const newWord = generateWord(6);
+    newWords.push(newWord);
+
+    setWords(newWords);
+  };
+
+  const handleMistake = () => {
+    setMistakes(mistakes + 1); // Zvýši počet chyb o jedna
+  };
+
+  useEffect(() => {
+    const initialWords = Array.from({ length: 3 }, () => generateWord(6));
+    setWords(initialWords);
+  }, []);
 
   return (
     <div className="stage">
-      <div className="stage__mistakes">Chyb: 0</div>
+      <div className="stage__mistakes">Chyb: {mistakes}</div>
       <div className="stage__words">
-        {words.map((word) => <Wordbox word={word} key={word} />)}
+        {words.map((word, index) => <Wordbox word={word} key={index} active={index === 0} onFinish={handleFinish} onMistake={handleMistake} />)}
       </div>
     </div>
   );
--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,12 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
-  
+const Wordbox = ({ word, onFinish, active, onMistake}) => {
+  const [lettersLeft, setLettersLeft] = useState(word);
+  const [mistake, setMistake] = useState(false); // Stav pro zjištění chyby
+
+  useEffect(() => {
+    const handleKeyUp = (event) => {
+      if (!active || lettersLeft === '') {
+        // slovo neni aktivni nebo je kompletni
+        return;
+      }
+
+      const userInput = event.key;
+      const currentLetter = lettersLeft[0];
+
+      if (userInput === currentLetter) {
+        const newLettersLeft = lettersLeft.slice(1); // odebere prvni pismeno
+        setLettersLeft(newLettersLeft);
+        setMistake(false);
+      } else {
+        setMistake(true);
+        onMistake();
+      }
+    };
+
+    if (active) {
+      document.addEventListener('keyup', handleKeyUp);
+    }
+
+    // pripojeni posluchače události keyUp na dokumentu
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      if (active) {
+        document.removeEventListener('keyup', handleKeyUp);
+      }
+    };
+  }, [lettersLeft, onFinish, active, onMistake]);
+
   return (
-    <div className="wordbox">{lettersLeft}</div>
+    <div className={`wordbox ${mistake ? 'wordbox--mistake' : ''}`}>{lettersLeft}</div>
   );
 };
 
 export default Wordbox;
+
